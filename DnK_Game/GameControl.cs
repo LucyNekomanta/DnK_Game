f42b253e912592d9f1d1d1aa477feb71b636875b
Karl Herbig <karl@herbig-burbach.de>
2021-06-13 14:32:36 +0200

    [control] add GameControl class and move all game elements there

---
+++ DnK_Game/GameControl.cs
@@ -0,0 +1,44 @@
+﻿using DnK_Game.quests;
+using DnK_Game.guild;
+using static System.Console;
+
+namespace DnK_Game
+{
+    public class GameControl
+    {
+        private Guild guild;
+        private QuestPool questBoard;
+
+        public GameControl()
+        {
+            // **** Quests ****
+            // Create a bunch of quests and store them in a QuestPool
+            questBoard = new QuestPool();
+            questBoard.Add(new Quest("Quest 1: Goblilns on the run!"));
+            questBoard.Add(new Quest("Quest 2: How to get away with murder?"));
+
+            foreach (var quest in questBoard.List)
+            {
+                WriteLine($"{quest.Name}");
+            }
+
+            // **** Guild ****
+            guild = new Guild("The Gamers");
+            WriteLine($"Foundation of the Guild \"{guild.Name}\"");
+        }
+
+        public void AcceptQuest(int questIdx)
+        {
+            if (questBoard.List.Count < questIdx)
+            {
+                WriteLine($"Can't accept Quest!\nquestIdx={questIdx}\nquest.List.Count={questBoard.List.Count}");
+                return;
+            }
+
+            var quest = questBoard.List[questIdx];
+            questBoard.Remove(quest);
+            guild.AddQuest(quest);
+            WriteLine($"The Guild \"{guild.Name}\" has accepted the quest \"{quest.Name}\"");
+        }
+    }
+}
--- DnK_Game/Program.cs
+++ DnK_Game/Program.cs
@@ -1,50 +1,16 @@
-﻿using DnK_Game.quests;
-using DnK_Game.guild;
-using static System.Console;
+﻿using static System.Console;
 
 namespace DnK_Game
 {
     class Program
     {
-        static public void AcceptQuest(Guild guild, QuestPool quests, int questIdx)
-        {
-            if (quests.List.Count < questIdx)
-            {
-                WriteLine($"Can't accept Quest!\nquestIdx={questIdx}\nquest.List.Count={quests.List.Count}");
-                return;
-            }
-
-            var quest = quests.List[questIdx];
-            quests.Remove(quest);
-            guild.AddQuest(quest);
-            WriteLine($"The Guild \"{guild.Name}\" has accepted the quest \"{quest.Name}\"");
-        }
-
         static void Main(string[] args)
         {
             WriteLine("Starting dnk_game");
 
-            // **** Quests ****
-            // Create a bunch of quests and store them in a QuestPool
-            var questBoard = new QuestPool();
-            questBoard.Add(new Quest("Quest 1: Goblilns on the run!"));
-            questBoard.Add(new Quest("Quest 2: How to get away with murder?"));
-            // Create a quest dummy to test the Remove method of the QuestPool
-            questBoard.Add(new Quest("Quest 3: This will be removed"));
-            questBoard.Remove(questBoard.List[2]);
+            GameControl gc = new GameControl();
 
-            // only Quest 1 and 2 should remain in the QuestPool
-            foreach (var quest in questBoard.List)
-            {
-                WriteLine($"{quest.Name}");
-            }
-
-            // **** Guild ****
-            var guild = new Guild("The Gamers");
-            WriteLine($"Foundation of the Guild \"{guild.Name}\"");
-
-            // **** Accepting a Quest ****
-            AcceptQuest(guild, questBoard, 0);
+            gc.AcceptQuest(0);
 
             ReadKey(true);
         }

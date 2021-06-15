bc08880cc157d02fda6716c9f7da8e27f69a0ee7
Karl Herbig <karl@herbig-burbach.de>
2021-06-15 20:43:52 +0200

    [DnkCharacter] add missing file

---
+++ DnK_Game/DnKCharacter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DnK_Game
+{
+    [Serializable]
+    public class DnKCharacter
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+    }
+}
